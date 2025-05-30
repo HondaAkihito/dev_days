61c4819330e0ecb70ea90ed82930bcf16dacc5a2
HondaAkihito <honaki.and.1114@gmail.com>
2025-05-30 15:39:51 +0900

    feat: ログイン画面の日本語化

---
+++ database/migrations/2025_05_29_225329_create_counts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('counts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title', 255);
+            $table->date('started_at');
+            $table->date('completed_at')->nullable();
+            $table->boolean('is_completed')->default(false);
+            $table->string('image_path')->nullable();
+            $table->string('url')->nullable();
+            $table->text('memo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('counts');
+    }
+};
