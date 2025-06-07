41e22c7cdf7dc87010bb18a55c6bbbaa4954c518
HondaAkihito <honaki.and.1114@gmail.com>
2025-06-07 19:04:13 +0900

    fix: エラー画面のヘッダーデザインの修正

--- resources/views/components/errors/error-page.blade.php
+++ resources/views/components/errors/error-page.blade.php
@@ -11,14 +11,19 @@
 <body class="bg-gray-100 text-gray-800 flex flex-col min-h-screen overflow-y-scroll">
 
     {{-- ヘッダー --}}
-    <header class="bg-gray-200 shadow">
-        <div class="container mx-auto">
-            <div class="px-4 py-4 flex justify-between items-center">
-                <a href="{{ route('counts.index') }}">
-                    <h1 class="text-xl font-bold">{{ config('app.name') }}</h1>
-                </a>
-            </div>
-        </div>
+    <header class="bg-gray-200 border-b border-gray-100">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div class="flex justify-between h-16">
+              <div class="flex">
+                  <!-- Logo -->
+                  <div class="shrink-0 flex items-center">
+                      <a href="{{ route('counts.index') }}">
+                          <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                      </a>
+                  </div>
+              </div>
+          </div>
+      </div>
     </header>
 
     {{-- メイン --}}
